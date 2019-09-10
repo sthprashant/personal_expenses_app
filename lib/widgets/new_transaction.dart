d51034b7821f1a4b830617b9496d8d6d93eedbc7
Prashant Shrestha <sthprashant@gmail.com>
2019-09-10 18:40:18 +1000

    improving and styling the modal bootom sheet--> basically just making sure the modal closes when submitted

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleContoller = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleContoller = TextEditingController();
+
+  final amountController = TextEditingController();
+
   void submitData() {
     final enteredTitle = titleContoller.text;
     final enteredAmount = double.parse(amountController.text);
@@ -14,10 +22,12 @@ class NewTransaction extends StatelessWidget {
     return;
     }
 
-    addTx(
+    widget.addTx(
       enteredTitle,
       enteredAmount,
     );
+
+    Navigator.of(context).pop();
 
   }
 
