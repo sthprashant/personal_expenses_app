3b789842a3f1f99f6c5c4b130bdf94c0e07982b5
Prashant Shrestha <sthprashant@gmail.com>
2019-09-14 22:03:52 +1000

    getting started with date picker

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -18,8 +18,8 @@ class _NewTransactionState extends State<NewTransaction> {
     final enteredTitle = titleContoller.text;
     final enteredAmount = double.parse(amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount<=0){
-    return;
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
     }
 
     widget.addTx(
@@ -28,7 +28,6 @@ class _NewTransactionState extends State<NewTransaction> {
     );
 
     Navigator.of(context).pop();
-
   }
 
   @override
@@ -57,9 +56,28 @@ class _NewTransactionState extends State<NewTransaction> {
               //   amountInput = val;
               // },
             ),
-            FlatButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Text('No Date Chosen!'),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            RaisedButton(
               child: Text('Add Transaction'),
-              textColor: Colors.purple,
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color ,
               onPressed: submitData,
             )
           ],
